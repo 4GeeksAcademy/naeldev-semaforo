3536cbe87cd6415f92a1e84db3804a6ec3a2e561
Nael <naeldiaz@proton.me>
2025-04-23 19:30:51 +0000

    falta el ultimo boton

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,59 @@
 import React from "react";
+import { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-            
+	const [color, setColor] = useState("");
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+
+	const changeColor = () => {
+		setColor('red');
+		if (color === 'red') setColor('yellow');
+		if (color === 'yellow') setColor('green')
+		if (color === 'green') setColor('red')
+	}
+	const addColor = () => {
+			
+	}
+
+
+	return (
+		<>
+			<div className="d-flex row  justify-content-center square my-5">
+				<div onClick={() => setColor('red')}
+					className={"color-first red mx-2" + (color === 'red' ? ' shadow-ch' : "")}>
+
+				</div>
+
+				<div onClick={() => setColor('yellow')}
+					className={"color-first yellow mx-2" + (color === 'yellow' ? ' shadow-ch' : "")}>
+
+				</div>
+
+				<div onClick={() => setColor('green')}
+					className={"color-first green mx-2" + (color === 'green' ? ' shadow-ch' : "")}>
+
+				</div>
+
+			</div>
+			<div className="d-flex justify-content-center mx-2">
+				<button onClick={changeColor}
+					type="button"
+					className="btn btn-primary mx-2">
+					Switch Color
+
+				</button>
+				<button onClick={addColor}
+					type="button"
+					className="btn btn-success mx-2">
+					Add Color
+
+				</button>
+			</div>
+		</>
 	);
 };
 
